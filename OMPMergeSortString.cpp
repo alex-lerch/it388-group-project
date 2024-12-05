c60b18e07a7b4e18a60a34eff1aee616f3b19149
Alex Lerch <alex-lerch@hotmail.com>
2024-12-05 12:52:11 -0600

    cleaned up variable names for readability

--- OMPMergeSortString.cpp
+++ OMPMergeSortString.cpp
@@ -21,53 +21,53 @@ std::vector<std::string> OMPMergeSortString::sort(std::vector<std::string>& arr,
 void OMPMergeSortString::mergesort(vector<string> &arr, int nproc)
 {
     omp_set_num_threads(nproc);
-    int n = arr.size();
+    int arrSize = arr.size();
 
     #pragma parallel omp for if(arr.size()>1000)
-    for (int curr_size = 1; curr_size <= n - 1; curr_size = 2 * curr_size) {
-        for (int left_start = 0; left_start < n - 1; left_start += 2 * curr_size) {
-            int mid = min(left_start + curr_size - 1, n - 1);
-            int right_end = min(left_start + 2 * curr_size - 1, n - 1);
+    for (int curSize = 1; curSize <= arrSize - 1; curSize = 2 * curSize) {
+        for (int leftStart = 0; leftStart < arrSize - 1; leftStart += 2 * curSize) {
+            int mid = min(leftStart + curSize - 1, arrSize - 1);
+            int rightEnd = min(leftStart + 2 * curSize - 1, arrSize - 1);
 
-            merge(arr, left_start, mid, right_end);
+            merge(arr, leftStart, mid, rightEnd);
         }
     }
 }
 
 void OMPMergeSortString::merge(vector<string> &arr, int left, int mid, int right)
 {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
+    int leftNumItems = mid - left + 1;
+    int rightNumItems = right - mid;
 
-    vector<string> L(n1);
-    vector<string> R(n2);
+    vector<string> leftArr(leftNumItems);
+    vector<string> rightArr(rightNumItems);
 
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = arr[mid + 1 + j];
+    for (int i = 0; i < leftNumItems; i++)
+        leftArr[i] = arr[left + i];
+    for (int j = 0; j < rightNumItems; j++)
+        rightArr[j] = arr[mid + 1 + j];
 
     int i = 0, j = 0, k = left;
 
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
+    while (i < leftNumItems && j < rightNumItems) {
+        if (leftArr[i] <= rightArr[j]) {
+            arr[k] = leftArr[i];
             i++;
         } else {
-            arr[k] = R[j];
+            arr[k] = rightArr[j];
             j++;
         }
         k++;
     }
 
-    while (i < n1) {
-        arr[k] = L[i];
+    while (i < leftNumItems) {
+        arr[k] = leftArr[i];
         i++;
         k++;
     }
 
-    while (j < n2) {
-        arr[k] = R[j];
+    while (j < rightNumItems) {
+        arr[k] = rightArr[j];
         j++;
         k++;
     }
